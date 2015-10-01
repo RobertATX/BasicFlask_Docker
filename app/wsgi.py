5a918a49df9cb1e45162fc243fa6d61cf6db9e0c
Robert Donovan <admin@mixfin.com>
2015-09-30 20:28:49 -0500

    Updates

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,23 +1,7 @@
 __author__ = 'Robert Donovan'
 
-# -*- coding: utf-8 -*-
-
-# from flask import Flask
-
-# application = Flask(__name__)
-
 
 from twoweeks import app
 import twoweeks.config as config
 
 app.run(debug=config.DEBUG, host=config.HOST)  # @application.route('/', methods=['GET'])
-# def index():
-#    return 'Goodbye cruel world!'
-
-
-# def test():
-#    application.run(debug=True)
-
-
-# if __name__ == '__main__':
-#    test()
